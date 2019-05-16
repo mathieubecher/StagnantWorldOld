fe4e61d74d0899024a4f542735544db3b5766835
Mathieu BELKHENCHIR <mathieu.belkhenchir@charente-maritime.fr>
2019-05-16 09:00:51 +0200

    fix detectmoveinput and automatically keyboard detect

--- Assets/Script/CharacterController.cs
+++ Assets/Script/CharacterController.cs
@@ -90,15 +90,16 @@ public class CharacterController : SimpleController
                 move.x = Input.GetAxis("Horizontal");
                 move.y = Input.GetAxis("Vertical");
             }
+            else joystick = false;
         }
         else
         {
-            if (Input.GetKeyDown(InputConfig.Top) || (Input.GetKey(InputConfig.Top) && move.y == 0)) move.y = 1;
-            else if (Input.GetKeyDown(InputConfig.Bottom) || (Input.GetKey(InputConfig.Bottom) && move.y == 0)) move.y = -1;
+            if (Input.GetKeyDown(InputConfig.Top) || (Input.GetKey(InputConfig.Top))) move.y = 1;
+            else if (Input.GetKeyDown(InputConfig.Bottom) || (Input.GetKey(InputConfig.Bottom))) move.y = -1;
             else if ((!Input.GetKey(InputConfig.Bottom) && Move.y < 0) || (!Input.GetKey(InputConfig.Top) && move.y > 0)) move.y = 0;
 
-            if (Input.GetKeyDown(InputConfig.Right) || (Input.GetKey(InputConfig.Right) && move.x == 0)) move.x = 1;
-            else if (Input.GetKeyDown(InputConfig.Left) || (Input.GetKey(InputConfig.Left) && move.x == 0)) move.x = -1;
+            if (Input.GetKeyDown(InputConfig.Right) || (Input.GetKey(InputConfig.Right))) move.x = 1;
+            else if (Input.GetKeyDown(InputConfig.Left) || (Input.GetKey(InputConfig.Left))) move.x = -1;
             else if ((!Input.GetKey(InputConfig.Left) && move.x < 0) || (!Input.GetKey(InputConfig.Right) && move.x > 0)) move.x = 0;
         }
         float actualspeed = (float)Math.Sqrt(Math.Pow(Move.x, 2) + Math.Pow(Move.y, 2));
