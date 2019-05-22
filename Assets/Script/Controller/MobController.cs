f68aa0dcf8848ac1b4b3e6910d0ccf2d0e16eb25
Becher <mbelkhenchir@gmail.com>
2019-05-22 18:59:34 +0200

    recalculate a*

--- Assets/Script/Controller/MobController.cs
+++ Assets/Script/Controller/MobController.cs
@@ -7,7 +7,7 @@ public class MobController : CharacterController
     private Vector3 target;
     public JoeStarState joestar;
     public bool isTarget = false;
-    public float speed = 0.05f;
+    public float speed = 0.1f;
     protected override void Start()
     {
         base.Start();
@@ -26,8 +26,8 @@ public class MobController : CharacterController
             target.z = 0;
             joestar = JoeStar();
             joestar.Next();
-            DrawLine.NewLine(target, joestar.node.transform.position, Color.red, 5);
-            DrawRoute(joestar);
+            //DrawLine.NewLine(target, joestar.node.transform.position, Color.red, 5);
+            //DrawRoute(joestar);
 
         }
     }
@@ -35,6 +35,7 @@ public class MobController : CharacterController
     // Détection du déplacement
     protected override void DetectMoveInput()
     {
+        
         if (joestar != null)
         {
             Node n = joestar.GetLastNode();
@@ -43,26 +44,35 @@ public class MobController : CharacterController
         else move = target - transform.position;
 
         float distance = move.magnitude;
-
-        if (distance < 0.01f)
+        if (joestar != null || distance > 0.01f)
         {
-            if (joestar != null && joestar.parent != null && joestar.parent.parent == null) joestar = null;
-            else if (joestar != null && joestar.parent != null)
+
+        
+            if (distance < 0.01f)
             {
-                joestar.Next();
-                DetectMoveInput();
+                if (joestar != null && joestar.parent != null && joestar.parent.parent == null) joestar = null;
+                else if (joestar != null && joestar.parent != null)
+                {
+                    joestar = JoeStar();
+                    joestar.Next();
+                    DetectMoveInput();
+                }
+                else joestar = null;
             }
-            else joestar = null;
-        }
-        else if (distance < speed)
-        {
-            Debug.Log("less than max move");
-            transform.position += new Vector3(move.x, move.y);
-            move = Vector3.zero;
+            else if (distance < speed/50)
+            {
+                Debug.Log("less than max move");
+                transform.position += new Vector3(move.x, move.y);
+                move = Vector3.zero;
 
+            }
+            else move = move * speed / distance;
+            Debug.Log(Node.distance / (float)10);
         }
-        else move = move * speed / distance;
-        Debug.Log(Node.distance / (float)10);
+        else
+        {
+            target = transform.position;
+        }
     }
 
     private JoeStarState JoeStar()
